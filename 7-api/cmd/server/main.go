2028c86ee91d87883be384f5af147df1270304de
Mateus Silva <mateussilva@ntopus.com.br>
2023-11-04 10:32:31 -0300

    Aula 7 - API - Crud de products

--- 7-api/cmd/server/main.go
+++ 7-api/cmd/server/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/mateus-sousa/goexpert/7-api/configs"
+	"github.com/mateus-sousa/goexpert/7-api/internal/entity"
+	"github.com/mateus-sousa/goexpert/7-api/internal/infra/database"
+	"github.com/mateus-sousa/goexpert/7-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+)
 
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDb := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDb)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products", productHandler.GetProducts)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	http.ListenAndServe(":8000", r)
 }
---
+++ 7-api/internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,125 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"github.com/mateus-sousa/goexpert/7-api/internal/dto"
+	"github.com/mateus-sousa/goexpert/7-api/internal/entity"
+	"github.com/mateus-sousa/goexpert/7-api/internal/infra/database"
+	entityPkg "github.com/mateus-sousa/goexpert/7-api/pkg/entity"
+	"net/http"
+	"strconv"
+)
+
+type ProductHandler struct {
+	repository database.ProductInterface
+}
+
+func NewProductHandler(repository database.ProductInterface) *ProductHandler {
+	return &ProductHandler{repository: repository}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var productDto dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&productDto)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := entity.NewProduct(productDto.Name, productDto.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusConflict)
+		return
+	}
+	err = h.repository.Create(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
+	page := r.URL.Query().Get("page")
+	pageInt, err := strconv.Atoi(page)
+	if err != nil {
+		pageInt = 0
+	}
+	limit := r.URL.Query().Get("limit")
+	limitInt, err := strconv.Atoi(limit)
+	if err != nil {
+		limitInt = 0
+	}
+	sort := r.URL.Query().Get("sort")
+	products, err := h.repository.FindAll(pageInt, limitInt, sort)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(products)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	product, err := h.repository.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var product entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product.ID, err = entityPkg.ParseID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	_, err = h.repository.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	err = h.repository.Update(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *ProductHandler) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	_, err := h.repository.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	err = h.repository.Delete(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
